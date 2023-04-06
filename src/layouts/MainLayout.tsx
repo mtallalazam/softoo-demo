0c82b174d1d4fadf87eefb76ddcd6f6b9bd1ec5f
Muhammad Tallal Azam <m.tallalazam@gmail.com>
2023-04-06 11:18:16 +0500

    Removed dark keyword

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -2,7 +2,7 @@ import { Outlet } from 'react-router-dom';
 
 const MainLayout = () => {
   return (
-    <main className="min-h-screen dark:bg-gray-900 dark:text-white">
+    <main className="min-h-screen bg-gray-900 text-white">
       <section className="mx-auto w-full max-w-[900px] p-6">
         <header className="mb-10">
           <h1 className="text-center text-4xl">
